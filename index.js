7cee079e3a37d9ac9e2e2ae175e6d5c07047fdd6
Dmitry Bardt <dq.ximet@gmail.com>
2017-06-11 12:14:55 +0300

    update style lib

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ module.exports = function(input) {
   	let success = false;
 
   	if (document.execCommand) {
-    	const item = document.createElement('input');
+        const item = document.createElement('input');
 
-    	item.value = input;
-    	item.setAttribute('readonly', '');
+    	  item.value = input;
+    	  item.setAttribute('readonly', '');
         item.style.position = 'absolute';
         item.style.left = '-1000px';
         item.style.fontSize = '14px';
-    	document.body.appendChild(item);
-    	item.select();
+    	  document.body.appendChild(item);
+    	  item.select();
 
-    	try {
-    	    success = document.execCommand('copy');
-    	}
+    	  try {
+    	      success = document.execCommand('copy');
+    	  }
         catch (err) {
             console.log('Not Supported in your browser!')
         }
 
-    	document.body.removeChild(item);
+    	  document.body.removeChild(item);
   	}
 
   	return success;
