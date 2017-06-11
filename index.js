ff4879003f6a71c1206ffeaab4fe5bf10d539435
ximet <dq.ximet@gmail.com>
2017-06-11 12:18:03 +0300

    delete not needed empty space

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 module.exports = function(input) {
-  	let success = false;
+  let success = false;
 
-  	if (document.execCommand) {
-        const item = document.createElement('input');
+  if (document.execCommand) {
+    const item = document.createElement('input');
 
-    	  item.value = input;
-    	  item.setAttribute('readonly', '');
-        item.style.position = 'absolute';
-        item.style.left = '-1000px';
-        item.style.fontSize = '14px';
-    	  document.body.appendChild(item);
-    	  item.select();
+    item.value = input;
+    item.setAttribute('readonly', '');
+    item.style.position = 'absolute';
+    item.style.left = '-1000px';
+    item.style.fontSize = '14px';
+    document.body.appendChild(item);
+    item.select();
 
-    	  try {
-    	      success = document.execCommand('copy');
-    	  }
-        catch (err) {
-            console.log('Not Supported in your browser!')
-        }
+    try {
+        success = document.execCommand('copy');
+    }
+    catch (err) {
+        console.log('Not Supported in your browser!')
+    }
 
-    	  document.body.removeChild(item);
-  	}
+    document.body.removeChild(item);
+  }
 
-  	return success;
+  return success;
 };
