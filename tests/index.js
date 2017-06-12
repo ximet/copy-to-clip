a3cb2fac8f8f3cc45dc0934cbd30f56a34cffc47
ximet <dq.ximet@gmail.com>
2017-06-12 19:08:45 +0300

    update test list

--- tests/index.js
+++ tests/index.js
@@ -4,13 +4,27 @@ const copyToClip = require('../index.js')
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-describe('SimpleTest', () => {
-    it('Test with fake dom', () => {
-        global.document = new JSDOM(undefined);
-        global.window = document.defaultView;
+describe('List Test', () => {
 
-        const param = copyToClip('text');
+    describe('Fake Test', () => {
+        it('Test with fake dom', () => {
+            global.document = new JSDOM(undefined);
+            global.window = document.defaultView;
 
-        expect(param).to.equal(false);
+            const param = copyToClip('text');
+
+            expect(param).to.equal(false);
+        });
+    });
+
+    describe('Good DOM Test', () => {
+        it('Create custom DOM with one inserted element', () => {
+            global.document = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+            global.window = document.defaultView;
+
+            const param = copyToClip('text');
+
+            expect(param).to.equal(false);
+        });
     });
 });
